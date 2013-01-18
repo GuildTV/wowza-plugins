cf2b32293c796d5eb29ea2e58fbc5b521e825c13
Julian Waller <big.big.julez@gmail.com>
2013-01-18 01:49:07 +0000

    removed junk

--- examples/stats/database.sql
+++ examples/stats/database.sql
@@ -1,11 +1,3 @@
-CREATE TABLE `keys` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `key` varchar(50) NOT NULL,
-  `ip` varchar(15) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `key` (`key`)
-);
-
 CREATE TABLE `stats` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
   `wowzaId` int(20) NOT NULL,
